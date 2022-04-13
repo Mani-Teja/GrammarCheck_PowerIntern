297bb4fbaca0cb30aa1a73e5aa89c976740c7595
Mani_Teja <56339895+Mani-Teja@users.noreply.github.com>
2022-04-13 21:07:07 +0530

    Delete proj.py

--- proj.py
+++
@@ -1,8 +0,0 @@
-import spacy
-
-doc = spacy.load("en_core_web_sm")
-doc.add_pipe("merge_entities")
-data = input("enter sentence : ")
-doc.add_pipe("merge_noun_chunks")
-for te in doc(data) :
-    print(te.text , te.pos_)
