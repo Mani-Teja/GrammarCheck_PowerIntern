8b0ccec39e4b155be1af2e70c8656bac0304dc55
Aviral Mishra <88927802+aviral-mishra-hunter@users.noreply.github.com>
2022-04-18 15:07:24 +0530

    Update spell.py

--- Grammar_checker/util/spell.py
+++ Grammar_checker/util/spell.py
@@ -10,8 +10,8 @@ def spell_checker(data):
     misspelled = word_tokenize(data)
     mispelled , err_count ="" , 0
     for word in misspelled:
-        corr_word = spell.correction(word)
-        if  word != corr_word and not word.isupper() and word not in ['.','"',"'",'?','!',':',';','(',')','[',']','{','}',',']:
+        corr_word = spell.correction(word)     #correcting spelling error
+        if  word != corr_word and not word.isupper() and word not in ['.','"',"'",'?','!',':',';','(',')','[',']','{','}',',']:   #if condition for checking if token is not in uppercase or any for the following punctuation marks.
             mispelled=mispelled+corr_word+" "
             err_count += 1
         else :
