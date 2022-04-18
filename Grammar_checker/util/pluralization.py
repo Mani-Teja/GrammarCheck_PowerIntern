ebae506e74a932001ec364e89756f968c1e5c807
Aviral Mishra <88927802+aviral-mishra-hunter@users.noreply.github.com>
2022-04-18 15:32:07 +0530

    Update pluralization.py

--- Grammar_checker/util/pluralization.py
+++ Grammar_checker/util/pluralization.py
@@ -7,7 +7,7 @@ from pattern.text.en import pluralize, singularize
         O/P:There are many chocolates in the box.
 """
 def check_pluralization(nlp):
-    count=0
+    error_count=0
     correct_text=""
     for s in nlp:
         for i in range(len(s)):
@@ -15,7 +15,7 @@ def check_pluralization(nlp):
                 continue
             if (i!=len(s)-1) and (s[i][1] in ['NN' , 'NNP']):                       #if condition for checking if the token encountered is singular noun or a singular pronoun.
                 if s[i+1][1] in ['VB','VBP'] or s[i-1][1] in ['VB','VBP','JJ']:     #if condition for checking if the singular noun or pronoun encountered is being followed by a (verb or singular verb in present tense) or has a (verb or singular verb in present tense or adjective) as its previous token. 
-                    count+=1
+                    error_count+=1
                     correct_text+=pluralize(s[i][0])+" "
                 else:
                     correct_text+=s[i][0]+" "
@@ -27,12 +27,12 @@ def check_pluralization(nlp):
             elif  (i!=len(s)-1) and s[i][1] in ['CD']:         #if condition for checking if the token encountered is a cardinal digit.
                 if s[i][0]=="1" or s[i][0]=="one":             #if condition for checking if the cardinal digit encountered is one(1).
                     if s[i+1][1] in ['NNS' , 'NNPS']:          #if condition for checking if the cardinal digit encountered is being followed by a plural noun or plural pronoun.
-                        count+=1
+                        error_count+=1
                         correct_text+=s[i][0]+" "+singularize(s[i+1][0])+" "
                 else:
                     if s[i+1][1] in ['NN' , 'NNP']:            #if condition for checking if the cardinal digit encountered is being followed by a singular noun or singular pronoun.
-                        count+=1
+                        error_count+=1
                         correct_text+=s[i][0]+" "+pluralize(s[i+1][0])+" "
             else:
                 correct_text+=s[i][0]+" "
-    return count,correct_text
+    return error_count,correct_text
