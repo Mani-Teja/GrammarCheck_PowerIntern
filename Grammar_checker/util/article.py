022cdf87fa639bafbb3b3b872bd68a2575bd82dc
chakori22 <61592791+chakori22@users.noreply.github.com>
2022-04-18 14:37:53 +0530

    Update article.py

--- Grammar_checker/util/article.py
+++ Grammar_checker/util/article.py
@@ -29,9 +29,16 @@ def check_articleError(nlp):
                 if ((sent[i][0] in unc_words) or sent[i+1][1] in ["NNS" ,"NNPS"] ):
                     count+=1
                 elif (i<len(sent)-2) and (sent[i+1][1] in ["JJ","JJR"]) and (sent[i+2][1] in ['NNP','NN']):
+                    """refernced provides appropriate article before the word 
+                    eg:
+                        I/P: referenced(hour)
+                        O/P: an hour
+                    """    
+                    #to check whether (i)th word equals 'a'  and refernce of next word equals 'an '+(i+1)th word     
                     if (sent[i][0]=='a' and referenced(sent[i+1][0])==('an '+sent[i+1][0])):
                         ntext+='an'
                         count+=1
+                    #to check whether (i)th word equals 'an'  and refernce of next word equals 'a '+(i+1)th word    
                     elif(sent[i][0]=='an' and referenced(sent[i+1][0])==('a '+sent[i+1][0])):
                         ntext+='a'
                         count+=1
