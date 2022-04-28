8570d6f2d4f449c1f0dfac35539737874ee69e45
Mani_Teja <56339895+Mani-Teja@users.noreply.github.com>
2022-04-28 10:14:53 +0530

    Update subVerb.py

--- Grammar_checker/util/subVerb.py
+++ Grammar_checker/util/subVerb.py
@@ -2,39 +2,39 @@
 Method developed by Sai maniteja Penugonda
 
 functionality : To check and modify the Subject Verb aggrement related errors provided in text.
-i/p Parameters : list of 2d matrix [[word , pos_tag],...]
+i/p Parameters : list of tuples [[word , pos_tag],...]
 o/p : (int , string) -> (error count , correct text)
 '''
 
 from pattern.en import conjugate
 import nltk
-from nltk.stem import WordNetLemmatizer
 
 def check_SubVerbAgreement(nlp):
     count=0
     text=""
-    lemmatizer= WordNetLemmatizer()
+    try :
+        conjugate('go','part')
+    except :
+        pass
     for s in nlp:
         for i in range(len(s)):
             try:
                 if s[i][1] in [ 'NN' , 'NNP'] :
                     #condition to check the next word is VERB
                     if (i!=len(s)-1) and (s[i+1][1] in ['VB','VBP','VBG']):
-                        verbLemma=lemmatizer.lemmatize(s[i+1][0])
-                        v=conjugate(verbLemma, tense = "present",person = 3, number = "singular", mood = "indicative",aspect = "imperfective",negated = False)
+                        v=conjugate(s[i+1][0], tense = "present",person = 3, number = "singular", mood = "indicative",aspect = "imperfective",negated = False)
                         text+=s[i][0]+" "
                         if s[i+1][0] != v :
+                            s[i+1] = (v,s[i+1][1])
                             count+=1
-                            s[i+1][0] = v
                         text += " "
                     # condition to check the next word is NOUN
                     elif (i!=len(s)-1) and (s[i+1][1] in ['NN','NNP']):
-                        verbLemma=lemmatizer.lemmatize(s[i+1][0])
-                        v=conjugate(verbLemma,tense = 'present' , person = 3 , number = 'singular' , mood = 'indicative' , aspect = 'imperfective' , negated = False)
+                        v=conjugate(s[i+1][0],tense = 'present' , person = 3 , number = 'singular' , mood = 'indicative' , aspect = 'imperfective' , negated = False)
                         text+=s[i][0]+" "
                         if s[i+1][0] != v :
+                            s[i+1] = (v,s[i+1][1])
                             count+=1
-                            s[i+1][0] = v
                         text += " "
                     else:
                         text+=s[i][0]
@@ -43,12 +43,11 @@ def check_SubVerbAgreement(nlp):
                 elif (i!=len(s)-1) and (s[i][1] in ['NNS' , 'NNPS']):
                     #condition to check the next word is VERB (present participle / 3rd person singular)
                     if s[i+1][1] in ['VBG','VBZ']:
-                        verbLemma=lemmatizer.lemmatize(s[i+1][0])
-                        v=conjugate(verbLemma, tense = "present",person = 3, number = "plural", mood = "indicative",aspect = "imperfective",negated = False)
+                        v=conjugate(s[i+1][0], tense = "present",person = 3, number = "plural", mood = "indicative",aspect = "imperfective",negated = False)
                         text+=s[i][0]+" "
                         if s[i+1][0] != v:
+                            s[i+1] = (v,s[i+1][1])
                             count+=1
-                            s[i+1][0]=v
                         text+=" "
                     else:
                         text+=s[i][0]
@@ -57,37 +56,34 @@ def check_SubVerbAgreement(nlp):
                 elif (i!=len(s)-1) and s[i][1] == 'PRP':
                     if s[i][0] == 'I':
                         #condition to check the next word is VERB (present participle/ past participle / 3rd person singular)
-                        if s[i+1][1] in ['VBZ','VBN','VBG']:                            
-                            verbLemma=lemmatizer.lemmatize(s[i+1][0])
-                            v=conjugate(verbLemma, tense = "present",person = 1, number = "singular", mood = "indicative",aspect = "imperfective",negated = False)
+                        if s[i+1][1] in ['VBZ','VBN','VBG']: 
+                            v=conjugate(s[i+1][0], tense = "present",person = 1, number = "singular", mood = "indicative",aspect = "imperfective",negated = False)
                             text+=s[i][0]+" "
                             if s[i+1][0]!=v:
+                                s[i+1] = (v,s[i+1][1])
                                 count+=1
-                                s[i+1][0]=v
                             text+=" "
                         else:
                             text+=s[i][0]
                             text+=" "
                     elif s[i][0].lower() in ['he','she','it']:
                         if s[i+1][1] in ['VBP','VB','VBN','VBG']:
-                            verbLemma=lemmatizer.lemmatize(s[i+1][0])
-                            v=conjugate(verbLemma, tense = "present",person = 3, number = "singular", mood = "indicative",aspect = "imperfective",negated = False)
+                            v=conjugate(s[i+1][0], tense = "present",person = 3, number = "singular", mood = "indicative",aspect = "imperfective",negated = False)
                             text+=s[i][0]+" "
                             if s[i+1][0] !=v:
+                                s[i+1] = (v,s[i+1][1])
                                 count+=1
-                                s[i+1][0] =v
                             text+=" "
                         else:
                             text+=s[i][0]
                             text+=" "
                     elif s[i][0].lower() in ['we','they','you']:
                         if s[i+1][1] not in ['VB','VBP']:
-                            verbLemma=lemmatizer.lemmatize(s[i+1][0])
-                            v=conjugate(verbLemma, tense = "present",person = 3, number = "plural", mood = "indicative",aspect = "imperfective",negated = False)
+                            v=conjugate(s[i+1][0], tense = "present",person = 3, number = "plural", mood = "indicative",aspect = "imperfective",negated = False)
                             text+=s[i][0]+" "
                             if s[i+1][0] !=v:
+                                s[i+1] = (v,s[i+1][1])
                                 count+=1
-                                s[i+1][0]=v
                             text+=" "
                         else:
                             text+=s[i][0]
@@ -99,6 +95,7 @@ def check_SubVerbAgreement(nlp):
                     text+=s[i][0]
                     text+=" "
             except Exception as e:
+                print(e)
                 text+=s[i][0]
                 text+=" "
     return count,text
