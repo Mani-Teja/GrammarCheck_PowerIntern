e32b7d10bac05a50c9bb47746d12f928e2ec4c52
king_on_fire <tpenugonda@gmail.com>
2022-05-22 22:46:29 +0530

    wrap up the project to final pip installable.

--- build/lib/grammarChecker/util/__init__.py
+++
@@ -1,9 +0,0 @@
-def textFormatter(text):
-    formatter = ''
-    for word in text.split():
-        if word[0].isalnum() and (len(formatter) > 0 and formatter[-1] != "'"):
-            formatter += ' '
-        formatter += word
-    if formatter[-1] != '.':
-        formatter += '.'
-    return formatter
